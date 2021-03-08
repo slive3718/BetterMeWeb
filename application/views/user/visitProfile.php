13a45ee42c54a43daed8d5f403db1a61880d5cd1
Rexter Dayuta <rexterdayuta@gmail.com>
2021-03-08 23:13:05 +0800

    Update visitProfile.php

--- application/views/user/visitProfile.php
+++ application/views/user/visitProfile.php
@@ -199,7 +199,7 @@
                             <div class="tb">
                                 <div class="td" id="p-c-i"> <?php
                 if (isset($pic_status)){ ?>
-                     <img src="<?=base_url().'./uploads/profilepic/profile'.$id?>.jpg" class="avatar img-circle img-thumbnail" style="height:50px;width:50px"  alt="profile pic">
+                     <img src="<?=base_url().'./uploads/profilepic/profile'.$val->userId?>.jpg" class="avatar img-circle img-thumbnail" style="height:50px;width:50px"  alt="profile pic">
                <?php }else{
                    ?>
                     <img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" class="avatar img-circle img-thumbnail" style="height:50px;width:50px"  alt="profile pic">
