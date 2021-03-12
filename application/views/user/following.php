2b8abb3d5fe29cce9c97ca18af3f656de2436bf7
Ken Denzel <kendenzelcruz@gmail.com>
2021-03-12 14:17:13 +0800

    Update following.php

--- application/views/user/following.php
+++ application/views/user/following.php
@@ -19,7 +19,8 @@
         </div>
         <header>
             <div class="tb"> 
-                <div>   <a href="<?= base_url().'user/homepage/'?>">Better Me</a></div>
+                <div>   <a style="font-weight: bold" class="btn btn-s btn-success rounded"
+                href="<?= base_url().'user/homepage/'?>" >Better Me</a></div>
                 <div class="td" id="search-form">
                     <form method="get" action="#">
                         <input type="text" placeholder="Better Me Search">
