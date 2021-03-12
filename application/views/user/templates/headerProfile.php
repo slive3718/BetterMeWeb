d67e9470bdac2361eb3d3ef3ee6c717a2ec18028
Ken Denzel <kendenzelcruz@gmail.com>
2021-03-12 12:37:51 +0800

    Create headerProfile.php

---
+++ application/views/user/templates/headerProfile.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<title><?php if ($page_title){
+		echo "$page_title";
+	}?>
+	</title>
+</head>
+
+	
+	
+
+  
