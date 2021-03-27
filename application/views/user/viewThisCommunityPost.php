f765474f13e5453ea15b6697a89bd3d932bd8bfa
Ken Denzel <kendenzelcruz@gmail.com>
2021-03-28 00:35:27 +0800

    Update viewThisCommunityPost.php

--- application/views/user/viewThisCommunityPost.php
+++ application/views/user/viewThisCommunityPost.php
@@ -146,7 +146,7 @@ $current_user = $this->session->userdata('id');
                             <?php }?>
 
                             <textarea
-                                class="form-control ml-1 shadow-none textarea" wrap="hard" rows="10" cols="10"
+                                class="form-control ml-1 shadow-none textarea" wrap="hard" rows="8" cols="8"
                                 name="community_comment"
                                 readonly="readonly"><?=  $user_comments?></textarea>
 
@@ -202,7 +202,7 @@ $current_user = $this->session->userdata('id');
                             <img class="fa fa-user">
                             <?php }?>
                             <textarea
-                                class="form-control ml-1 shadow-none textarea" wrap="hard" rows="10" cols="10"
+                                class="form-control ml-1 shadow-none textarea" wrap="hard" rows="8" cols="8"
                                 name="community_comment"></textarea>
 
                         </div>
