b349f1198081bca2dd1db12b7580b873c72c46d2
Ken Denzel <kendenzelcruz@gmail.com>
2021-04-03 17:23:24 +0800

    Update homepage.php

--- application/views/user/homepage.php
+++ application/views/user/homepage.php
@@ -184,7 +184,7 @@
 
 	<div class="threads col-4" style="float:center; max-width: 100%">
 		<div class="shadow-lg p-3 mb-5 ml-5 responsive"
-			 style="display:inline-block;right:20px;" >
+			 style="display:inline-block;" >
 				<?php if ($this->session->flashdata('msgsuccess_c')) {
 					echo "<div class='btn btn-success'>" . $this->session->flashdata('msgsuccess_c') . '</div>';
 				} ?>
