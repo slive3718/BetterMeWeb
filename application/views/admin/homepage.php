2e745669a77145f4fcb8a6b97087a48a4af2647c
Ken Denzel <kendenzelcruz@gmail.com>
2021-04-03 18:43:58 +0800

    Update homepage.php

--- application/views/admin/homepage.php
+++ application/views/admin/homepage.php
@@ -132,17 +132,17 @@
 						<div>Date Posted: <?= $date_posted ?></div>
 					</div>
 
-					<div class="card responsive" style="width:30rem;height:30rem">
+					<div class="card responsive" style="width:23rem;height:30rem">
 
-						<a href="<?= base_url('admin/viewFullDiet/' . $post_id) ?>">
-							<div class="container">
-								<?php foreach ($row->images as $images) {
-									?>
-									<img class="" src="<?= base_url() . 'uploads/posts/' . $images->image_name ?>"
-										 alt="Card image cap"  style="max-height:100px;max-width:120px">
-								<?php } ?>
-							</div>
-						</a>
+							<a href="<?= base_url('admin/viewFullDiet/' . $post_id) ?>">
+								<div class="container" >
+									<?php foreach ($row->images as $images) {
+										?>
+										<img class="image" src="<?= base_url() . 'uploads/posts/' . $images->image_name ?>"
+											 alt="Card image" style="max-height:100px;max-width:120px">
+									<?php } ?>
+								</div>
+							</a>
 
 						<div class="card-body" style="">
 							<div class="rounded card shadow"
