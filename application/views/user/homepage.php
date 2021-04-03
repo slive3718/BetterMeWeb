7b67105fa6518981d07ca4cc48bfae68ec1aa324
Ken Denzel <kendenzelcruz@gmail.com>
2021-04-03 16:46:32 +0800

    Update homepage.php

--- application/views/user/homepage.php
+++ application/views/user/homepage.php
@@ -183,7 +183,7 @@
 	</div>
 
 
-	<div class="threads col-4" style="float:left;">
+	<div class="threads col-4" style="float:right;max-width: 100%">
 		<div class="shadow-lg p-3 mb-5 ml-5 responsive"
 			 style="display:inline-block;right:20px;" >
 				<?php if ($this->session->flashdata('msgsuccess_c')) {
