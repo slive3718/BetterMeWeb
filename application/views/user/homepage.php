28f66e0e6a55ff74c96441b9633d17a95604b856
Ken Denzel <kendenzelcruz@gmail.com>
2021-04-03 18:06:23 +0800

    Update homepage.php

--- application/views/user/homepage.php
+++ application/views/user/homepage.php
@@ -124,10 +124,12 @@
 							<img style="width:2rem;height:2rem" ;
 								 src="<?= base_url() . './uploads/profilepic/profile' . $post_user_id ?>.jpg"
 								 class="img-circle img-responsive"
-								 alt=""/> Posted by: <?= ucfirst($posts_user_name) ?></div>
+								 alt=""/> Posted by: <?= ucfirst($posts_user_name) ?>
+								 Date Posted: <?= $date_posted ?>
+								 </div>
 						<?php
 						} ?>
-						Date Posted: <?= $date_posted ?>
+						
 					</div>
 
 					<div class="card responsive" style="width:23rem;height:30rem">
