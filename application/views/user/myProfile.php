78b34a1fd2e390d2584ad8a70c92751a0a561e40
Ken Denzel <kendenzelcruz@gmail.com>
2021-04-08 02:50:00 +0800

    Update myProfile.php

--- application/views/user/myProfile.php
+++ application/views/user/myProfile.php
@@ -89,6 +89,7 @@
 	<div id="main-content">
 		<div class="tb ">
 			<div class="td" id="l-col">
+			<div class="td" id="m-col">
 				<div class="m-mrg card" id="p-tabs">
 					<div class="tb">
 						<div class="td">
