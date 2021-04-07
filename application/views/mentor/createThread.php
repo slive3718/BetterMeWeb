6d8c5173690d9d4f22a3f882f337f88db8a9c120
Ken Denzel <kendenzelcruz@gmail.com>
2021-04-08 01:51:38 +0800

    Update createThread.php

--- application/views/mentor/createThread.php
+++ application/views/mentor/createThread.php
@@ -41,7 +41,7 @@
     		        <button type="submit" class="btn btn-primary">
     		            Create
     		        </button>
-    		        <a href="<?php echo base_url('mentor/homepage')?>"><button>
+    		        <a href="<?php echo base_url('mentor/homepage')?>"><button class="btn btn-default">
     		            Cancel
 					</button></a>
     		    </div>
