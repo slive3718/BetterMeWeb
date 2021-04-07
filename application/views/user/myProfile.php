096bf685af0b1cc1cfcf2058b87656a1b394a0ed
Ken Denzel <kendenzelcruz@gmail.com>
2021-04-08 03:19:46 +0800

    Update myProfile.php

--- application/views/user/myProfile.php
+++ application/views/user/myProfile.php
@@ -89,6 +89,7 @@
 	<div id="main-content">
 		<div class="tb">
 			<div class="td" id="1-col">
+			<div class="td" id="m-col">
 			<div class="m-mrg card Regular shadow" id="composer"  style='right:20px'>
 				<div class="m-mrg card" id="p-tabs">
 					<div class="tb" >
