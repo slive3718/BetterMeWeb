11548042373c6b951799663c98d80d758dea1ccc
Ken Denzel <kendenzelcruz@gmail.com>
2021-04-08 03:02:36 +0800

    Update myProfile.php

--- application/views/user/myProfile.php
+++ application/views/user/myProfile.php
@@ -90,7 +90,7 @@
 			<div class="td" id="l-col">
 			<div class="td" id="m-col">
 				<div class="m-mrg card" id="p-tabs">
-					<div class="tb" style="margin:20px;">
+					<div class="tb">
 						<div class="td">
 							<div class="tb" id="p-tabs-m">
 								<div class="td active"><a href="<?= base_url() . 'user/myProfile' ?>" class=""><i
