601c125d4980e8bb4e603c2b97f59d086c2b1fed
Ken Denzel <kendenzelcruz@gmail.com>
2021-04-07 23:55:31 +0800

    Update homepage.php

--- application/views/user/homepage.php
+++ application/views/user/homepage.php
@@ -119,7 +119,7 @@
 				<div class="shadow p-3 mb-5" style='float:left;margin-left:15px;margin-bottom:30px;'>
 					<div class="d-flex justify-content-between btn btn-success btn-xs">
 
-						<div style="font-weight: bold;">    <?php if (isset($pic_status)){
+						<div style="font-size: 10px; font-weight: bold;">    <?php if (isset($pic_status)){
 							?>
 							<img style="width:2rem;height:2rem" ;
 								 src="<?= base_url() . './uploads/profilepic/profile' . $post_user_id ?>.jpg"
@@ -127,7 +127,7 @@
 								 alt=""/> Posted by: <?= ucfirst($posts_user_name) ?></div>
 						<?php
 						} ?>
-						<div style=" font-weight: bold;">Date Posted: <?= $date_posted ?></div>
+						<div style="font-size: 10px; font-weight: bold;">Date Posted: <?= $date_posted ?></div>
 					</div>
 
 					<div class="card responsive class-card">
