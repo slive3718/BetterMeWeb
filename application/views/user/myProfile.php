5c15890e1b91e3cc2a73a71eb8c3200b212b00b2
Ken Denzel <kendenzelcruz@gmail.com>
2021-04-08 03:07:47 +0800

    Update myProfile.php

--- application/views/user/myProfile.php
+++ application/views/user/myProfile.php
@@ -87,6 +87,8 @@
 		<div id="black-grd"></div>
 	</div>
 		<div class="tb" style="float: center;">
+			<div class="td" id="5-col">
+			<div class="td" id="m-col">
 				<div class="m-mrg card" id="p-tabs">
 					<div class="tb">
 						<div class="td">
