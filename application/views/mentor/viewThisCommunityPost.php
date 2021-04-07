a97cbef5aba2cac99eeea25b68b235ebcc6a3163
Ken Denzel <kendenzelcruz@gmail.com>
2021-04-08 00:47:43 +0800

    Update viewThisCommunityPost.php

--- application/views/mentor/viewThisCommunityPost.php
+++ application/views/mentor/viewThisCommunityPost.php
@@ -171,7 +171,7 @@ $current_user = $this->session->userdata('id');
                                 aria-expanded="true"
                                 aria-controls="collapse-1"
                                 href="#collapse-1">
-                                <i class="fa fa-commenting-o"></i>
+                                <i class="fa fa-commenting-o" style="cursor:pointer"></i>
                                 <span class="ml-1" style="cursor:pointer; font-weight: bold;">Write a Comment Here</span></div>
                         </div>
                     </div>
