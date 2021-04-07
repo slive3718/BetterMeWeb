663abe3674040bb7c64fd70b9d639508d16a49ac
Ken Denzel <kendenzelcruz@gmail.com>
2021-04-07 17:39:05 +0800

    Update headerProfile.php

--- application/views/user/templates/headerProfile.php
+++ application/views/user/templates/headerProfile.php
@@ -1,7 +1,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="shortcut icon" href="<?= base_url();?>assets/images/Smile.png">
 	
 	<title><?php if ($page_title){
