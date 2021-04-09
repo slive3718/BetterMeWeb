cb61043d11c61938b40754a3abdb29aed8b07229
Ken Denzel <kendenzelcruz@gmail.com>
2021-04-09 15:41:04 +0800

    Update myProfile.php

--- application/views/user/myProfile.php
+++ application/views/user/myProfile.php
@@ -131,7 +131,7 @@
 									name="content"
 									placeholder="What's on your mind?"
 									class="whats-on-ur-mind border border-primary rounded"
-									cols="150"
+									cols="100"
 									rows="5" required></textarea>
 
 								</div>
