64afae733017704422b4a4efa020f78501b4d7ad
Rexter Dayuta <rexterdayuta@gmail.com>
2021-04-10 22:04:46 +0800

    update header remove unnecessary style

--- application/views/user/templates/header.php
+++ application/views/user/templates/header.php
@@ -3,7 +3,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css"
 
-		  href="<?php echo base_url(); ?>assets/css/bootstrap.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
 		  integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
 		  crossorigin="anonymous"/>
@@ -22,43 +21,17 @@
 	<!-- end: META -->
 
 	<!-- start: GOOGLE FONTS -->
-	<link rel="stylesheet" href="<?= base_url() ?>assets/css/googlefonts.css">
 	<link rel="shortcut icon" href="<?= base_url();?>assets/images/Smile.png">
-	<!--<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />-->
-	<!-- end: GOOGLE FONTS -->
-	<!-- start: MAIN CSS -->
+
+
 	<link rel="stylesheet" href="<?= base_url() ?>assets/vendor/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" href="<?= base_url() ?>assets/vendor/fontawesome/css/font-awesome.min.css">
-	<link rel="stylesheet" href="<?= base_url() ?>assets/vendor/themify-icons/themify-icons.min.css">
-	<link href="<?= base_url() ?>assets/vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
-	<link href="<?= base_url() ?>assets/vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet"
-		  media="screen">
-	<link href="<?= base_url() ?>assets/vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
-	<!-- end: MAIN CSS -->
-	<!-- start: CLIP-TWO CSS -->
-	<link rel="stylesheet" href="<?= base_url() ?>assets/css/styles.css">
-	<link rel="stylesheet" href="<?= base_url() ?>assets/css/plugins.css">
-	<link rel="stylesheet" href="<?= base_url() ?>assets/css/themes/theme-1.css" id="skin_color"/>
-	<!-- end: CLIP-TWO CSS -->
-	<!-- start: CSS REQUIRED FOR THIS PAGE ONLY -->
-	<!-- end: CSS REQUIRED FOR THIS PAGE ONLY -->
+
 
 	<link href="<?= base_url() ?>assets/vendor/select2/select2.min.css" rel="stylesheet" media="screen">
-	<link href="<?= base_url() ?>assets/vendor/DataTables/css/DT_bootstrap.css" rel="stylesheet" media="screen">
-	<link href="<?= base_url() ?>assets/vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css" rel="stylesheet"
-		  media="screen">
-	<link href="<?= base_url() ?>assets/vendor/bootstrap-datepicker/bootstrap-datepicker3.standalone.min.css"
-		  rel="stylesheet" media="screen">
-	<link href="<?= base_url() ?>assets/vendor/bootstrap-datetimepicker/bootstrap-datetimepicker.min.css"
-		  rel="stylesheet" media="screen">
 	<!-- iCheck for checkboxes and radio inputs -->
-	<link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/vendor/iCheck/all.css"/>
-	<link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/vendor/iCheck/minimal/blue.css"/>
-	<link rel="stylesheet" type="text/css" href="<?= base_url() ?>front_assets/css/custom.css" media="screen"/>
 
 	<link href="<?= base_url() ?>assets/alertify/alertify.core.css" rel="stylesheet" type="text/css"/>
 	<link href="<?= base_url() ?>assets/alertify/alertify.default.css" rel="stylesheet" type="text/css"/>
-	<link href="<?= base_url() ?>assets/css/myset.css" rel="stylesheet" type="text/css"/>
 	<!-- <link rel="stylesheet" type="text/css" href="assets/toggel/css/on-off-switch.css"/> -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
