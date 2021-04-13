63006ad6ba34f02902168af096121b29c36a745f
Ken Denzel <kendenzelcruz@gmail.com>
2021-04-13 20:01:30 +0800

    Update visitProfile.php

--- application/views/user/visitProfile.php
+++ application/views/user/visitProfile.php
@@ -89,7 +89,7 @@
 						 class="avatar img-circle img-thumbnail" style="height:225px;width:225px" alt="avatar">
 				<?php } else {
 					?>
-					<img src="https://www.linkpicture.com/q/images_292.jpeg" type="image" 
+					<img src="https://www.linkpicture.com/q/profileicon.png" type="image" 
 						class="avatar img-circle img-thumbnail" style="height:225px;width:225px" alt="avatar">
 					<?php
 				} ?>
