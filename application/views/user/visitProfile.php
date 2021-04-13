51e680de01d2d261728d26d594147ec7c83b9449
Ken Denzel <kendenzelcruz@gmail.com>
2021-04-13 20:08:17 +0800

    Update visitProfile.php

--- application/views/user/visitProfile.php
+++ application/views/user/visitProfile.php
@@ -41,202 +41,205 @@
 		<div class="tb">
 			<div><a href="<?php echo base_url('user/homepage') ?>">
 			<img src="<?= base_url()?>uploads/files/logo.png" style="width:200px;height:70px;"></a></div>
-			<?php foreach ($user_info as $val){
+<?php 
+if ($user_info){
+	foreach ($user_info as $val){
 
-			$id = $this->session->userdata('id');
-			$username = $val->username;
-			$firstName = $val->first_name;
-			$middleName = $val->middle_name;
-			$lastName = $val->last_name;
-			$email = $val->email;
-			$dob = $val->dob;
-			$pic_status = $val->user_picture_status;
-			$sex = $val->sex;
-			$userId = $val->userId;
-			?>
-			<div class="td" id="f-name-l"><a style="font-weight: bold" class="btn btn-s btn-success rounded"
-				href="<?= base_url() . 'user/myProfile/' . $id ?>">Home</a></div>
-			<div class="td" id="i-links">
+		$id = $this->session->userdata('id');
+		$username = $val->username;
+		$firstName = $val->first_name;
+		$middleName = $val->middle_name;
+		$lastName = $val->last_name;
+		$email = $val->email;
+		$dob = $val->dob;
+		$pic_status = $val->user_picture_status;
+		$sex = $val->sex;
+		$userId = $val->userId;
+		?>
+		<div class="td" id="f-name-l"><a style="font-weight: bold" class="btn btn-s btn-success rounded"
+			href="<?= base_url() . 'user/myProfile/' . $id ?>">Home</a></div>
+		<div class="td" id="i-links">
+			<div class="tb">
+				<div class="td">
+					<a href="#" id="p-link">
+						<?php if (isset($pic_status)) { ?>
+							<img src="<?= base_url() . './uploads/profilepic/profile' . $val->userId ?>.jpg"
+								 class="" style="height:35px;width:35px"
+								 alt="profile pic">
+						<?php } else {
+							?>
+							<img src="https://www.linkpicture.com/q/profileicon.png" type="image"
+								class="avatar img-circle img-thumbnail" style="height:35px;width:35px"
+								alt="profile pic">
+							<?php
+						} ?>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+<div id="profile-upper">
+	<div id="profile-banner-image">
+		<img src="https://imagizer.imageshack.com/img924/8210/3cMvFg.jpg" alt="Banner image">
+	</div>
+	<div id="profile-d">
+		<div id="profile-pic" class="card Regular shadow">
+			<?php
+			if (isset($pic_status)) { ?>
+				<img src="<?= base_url() . './uploads/profilepic/profile' . $val->userId ?>.jpg"
+					 class="avatar img-circle img-thumbnail" style="height:225px;width:225px" alt="avatar">
+			<?php } else {
+				?>
+				<img src="https://www.linkpicture.com/q/profileicon.png" type="image" 
+					class="avatar img-circle img-thumbnail" style="height:225px;width:225px" alt="avatar">
+				<?php
+			} ?>
+		</div>
+		<div id="u-name"><?= Ucfirst($firstName), ' ', Ucfirst($lastName) ?></div>
+		<div class="tb" id="m-btns">
+			<div class="td">
+				<!--<div class="m-btn"><i class="material-icons">Change Timeline Piture</i><span></span></div>-->
+			</div>
+		</div>
+	</div>
+	<div id="black-grd"></div>
+</div>
+<div id="main-content">
+		<div class="m-mrg card" id="composer"  style='margin:30px;'>
+			<div class="m-mrg card" id="p-tabs">
 				<div class="tb">
 					<div class="td">
-						<a href="#" id="p-link">
-							<?php if (isset($pic_status)) { ?>
+						<div class="tb" id="p-tabs-m">
+							<div class="td active"><a class=""><i
+											class="fa fa-clock-o"></i><span>TIMELINE</span></a></div>
+							<div class="td"><a class=""><i
+											class="fa fa-user-plus"></i><span>Following</span></a></div>
+							<div class="td"><a class=""><i
+											class="fa fa-users"></i><span>Followers</span> </a></div>
+						</div>
+					</div>
+					<!-- <div class="td" id="p-tab-m"><i class="material-icons">keyboard_arrow_down</i></div> -->
+				</div>
+			</div>
+			<div class="m-mrg card Regular shadow" id="composer" hidden>
+				<div id="c-tabs-cvr">
+					<div class="tb" id="c-tabs">
+						<div class="td"><i class="material-icons">Whats Up?</i><span></span></div>
+					</div>
+				</div>
+				<div id="c-c-main">
+					<div class="tb">
+						<div class="td" id="p-c-i"> <?php
+							if (isset($pic_status)) { ?>
 								<img src="<?= base_url() . './uploads/profilepic/profile' . $val->userId ?>.jpg"
-									 class="" style="height:35px;width:35px"
+									 class="avatar img-circle img-thumbnail" style="height:50px;width:50px"
 									 alt="profile pic">
 							<?php } else {
 								?>
 								<img src="https://www.linkpicture.com/q/profileicon.png" type="image"
-									class="avatar img-circle img-thumbnail" style="height:35px;width:35px"
-									alt="profile pic">
+									 class="avatar img-circle img-thumbnail" style="height:50px;width:50px"
+									 alt="profile pic">
 								<?php
-							} ?>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</header>
-	<div id="profile-upper">
-		<div id="profile-banner-image">
-			<img src="https://imagizer.imageshack.com/img924/8210/3cMvFg.jpg" alt="Banner image">
-		</div>
-		<div id="profile-d">
-			<div id="profile-pic" class="card Regular shadow">
-				<?php
-				if (isset($pic_status)) { ?>
-					<img src="<?= base_url() . './uploads/profilepic/profile' . $val->userId ?>.jpg"
-						 class="avatar img-circle img-thumbnail" style="height:225px;width:225px" alt="avatar">
-				<?php } else {
-					?>
-					<img src="https://www.linkpicture.com/q/profileicon.png" type="image" 
-						class="avatar img-circle img-thumbnail" style="height:225px;width:225px" alt="avatar">
-					<?php
-				} ?>
-			</div>
-			<div id="u-name"><?= Ucfirst($firstName), ' ', Ucfirst($lastName) ?></div>
-			<div class="tb" id="m-btns">
-				<div class="td">
-					<!--<div class="m-btn"><i class="material-icons">Change Timeline Piture</i><span></span></div>-->
-				</div>
-			</div>
-		</div>
-		<div id="black-grd"></div>
-	</div>
-	<div id="main-content">
-			<div class="m-mrg card" id="composer"  style='margin:30px;'>
-				<div class="m-mrg card" id="p-tabs">
-					<div class="tb">
-						<div class="td">
-							<div class="tb" id="p-tabs-m">
-								<div class="td active"><a class=""><i
-												class="fa fa-clock-o"></i><span>TIMELINE</span></a></div>
-								<div class="td"><a class=""><i
-												class="fa fa-user-plus"></i><span>Following</span></a></div>
-								<div class="td"><a class=""><i
-												class="fa fa-users"></i><span>Followers</span> </a></div>
+							} ?></div>
+						<form
+								method="post"
+								action="<?= base_url() ?>user/do_upload"
+								enctype="multipart/form-data">
+							<div class="Small" id="c-inp">
+						<textarea
+								name="content"
+								placeholder="What's on your mind?"
+								class="whats-on-ur-mind border border-primary rounded"
+								cols="100"
+								rows="5"></textarea>
+
 							</div>
-						</div>
-						<!-- <div class="td" id="p-tab-m"><i class="material-icons">keyboard_arrow_down</i></div> -->
 					</div>
+
+					<!-- <div id="insert_emoji"><a class="btn btn-primary btn-sm
+					button-post">Post</a></div> -->
 				</div>
-				<div class="m-mrg card Regular shadow" id="composer" hidden>
-					<div id="c-tabs-cvr">
-						<div class="tb" id="c-tabs">
-							<div class="td"><i class="material-icons">Whats Up?</i><span></span></div>
-						</div>
-					</div>
-					<div id="c-c-main">
-						<div class="tb">
-							<div class="td" id="p-c-i"> <?php
-								if (isset($pic_status)) { ?>
-									<img src="<?= base_url() . './uploads/profilepic/profile' . $val->userId ?>.jpg"
-										 class="avatar img-circle img-thumbnail" style="height:50px;width:50px"
-										 alt="profile pic">
-								<?php } else {
-									?>
-									<img src="https://www.linkpicture.com/q/profileicon.png" type="image"
-										 class="avatar img-circle img-thumbnail" style="height:50px;width:50px"
-										 alt="profile pic">
-									<?php
-								} ?></div>
-							<form
-									method="post"
-									action="<?= base_url() ?>user/do_upload"
-									enctype="multipart/form-data">
-								<div class="Small" id="c-inp">
-                            <textarea
-									name="content"
-									placeholder="What's on your mind?"
-									class="whats-on-ur-mind border border-primary rounded"
-									cols="100"
-									rows="5"></textarea>
-
-								</div>
-						</div>
-
-						<!-- <div id="insert_emoji"><a class="btn btn-primary btn-sm
-						button-post">Post</a></div> -->
-					</div>
-					</hr>
-					<div class="col-sm-12">
-						<input
-								class="btn btn-primary btn-sm col-md-4"
-								style="left:70px"
-								type="file"
-								name="userfile[]"
-								size="20"
-								multiple="multiple"/>
-						<input
-								type="submit"
-								value="Post"
-								class="btn btn-primary"
-								style="right:0px;float:right"/>
-					</div>
-					<!-- -->
+				</hr>
+				<div class="col-sm-12">
+					<input
+							class="btn btn-primary btn-sm col-md-4"
+							style="left:70px"
+							type="file"
+							name="userfile[]"
+							size="20"
+							multiple="multiple"/>
+					<input
+							type="submit"
+							value="Post"
+							class="btn btn-primary"
+							style="right:0px;float:right"/>
 				</div>
+				<!-- -->
+			</div>
 
-				<?php
+			<?php
 
-				if (isset($val->getAllProfilePost) && !empty($val->getAllProfilePost)) {
-					foreach ($val->getAllProfilePost as $post) {
+			if (isset($val->getAllProfilePost) && !empty($val->getAllProfilePost)) {
+				foreach ($val->getAllProfilePost as $post) {
 
-						$content = $post->content; ?>
-						<div class="m-mrg card Regular shadow" id="">
-							<div>
-								<div class="post card Regular shadow">
-									<div class="tb">
-										<a href="#" class="td p-p-pic"><?php
-											if (isset($pic_status)) { ?>
-												<img src="<?= base_url() . './uploads/profilepic/profile' . $userId ?>.jpg"
-													 class="avatar img-circle img-thumbnail"
-													 style="height:50px;width:50px" alt="profile pic">
-											<?php } else {
-												?>
-												<img src="https://www.linkpicture.com/q/profileicon.png" type="image"
-													 class="avatar img-circle img-thumbnail"
-													 style="height:50px;width:50px" alt="profile pic">
-												<?php
-											} ?></a>
-										<div class="td p-r-hdr">
-											<div class="p-u-info">
-												<a href="#"><?= Ucfirst($firstName), " ", Ucfirst($lastName) ?></a>
-											</div>
-											<div class="p-dt">
-												<i class="fa fa-calendar"></i>
-												<span>Date</span>
-											</div>
-
+					$content = $post->content; ?>
+					<div class="m-mrg card Regular shadow" id="">
+						<div>
+							<div class="post card Regular shadow">
+								<div class="tb">
+									<a href="#" class="td p-p-pic"><?php
+										if (isset($pic_status)) { ?>
+											<img src="<?= base_url() . './uploads/profilepic/profile' . $userId ?>.jpg"
+												 class="avatar img-circle img-thumbnail"
+												 style="height:50px;width:50px" alt="profile pic">
+										<?php } else {
+											?>
+											<img src="https://www.linkpicture.com/q/profileicon.png" type="image"
+												 class="avatar img-circle img-thumbnail"
+												 style="height:50px;width:50px" alt="profile pic">
+											<?php
+										} ?></a>
+									<div class="td p-r-hdr">
+										<div class="p-u-info">
+											<a href="#"><?= Ucfirst($firstName), " ", Ucfirst($lastName) ?></a>
 										</div>
-									</div>
-									<label class="tb " readonly>
-										<center><?= $content ?></center>
-									</label>
-									<div class="d-flex justify-content-center">
+										<div class="p-dt">
+											<i class="fa fa-calendar"></i>
+											<span>Date</span>
+										</div>
 
-										<a href="#" class="">
-											<div class="container">
-												<?php foreach ($post->post_images as $images) {
-													?>
-													<img src="<?= base_url() . './uploads/profile_posts/' . $images->image_name ?>">
-													<?php
-												} ?>
-											</div>
 									</div>
-									</a>
 								</div>
+								<label class="tb " readonly>
+									<center><?= $content ?></center>
+								</label>
+								<div class="d-flex justify-content-center">
+
+									<a href="#" class="">
+										<div class="container">
+											<?php foreach ($post->post_images as $images) {
+												?>
+												<img src="<?= base_url() . './uploads/profile_posts/' . $images->image_name ?>">
+												<?php
+											} ?>
+										</div>
+								</div>
+								</a>
 							</div>
 						</div>
-					<?php }}
-				 ?>
-				<div clas="fa-3x"><i class="fas fa-sync fa-spin"></i></div>
-			</div>
-
+					</div>
+				<?php }}
+			 ?>
+			<div clas="fa-3x"><i class="fas fa-sync fa-spin"></i></div>
 		</div>
+
 	</div>
-	<?php
+</div>
+<?php
 	}
-	?>
+}
+?>
 </main>
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
