b8f035e7a36ae8804854a8e920add07e93834d84
Ken Denzel <kendenzelcruz@gmail.com>
2021-04-14 17:52:29 +0800

    Update followedUser.php

--- application/views/user/followedUser.php
+++ application/views/user/followedUser.php
@@ -71,9 +71,16 @@
 	<div>
 		<div class="post card Regular shadow">
 			<div class="tb">
-				<a href="#" class="td p-p-pic">
+				<a href="#" class="td p-p-pic"> <?php
+					if (isset($pic_status)&& !empty($pic_status)) { ?>
 						<img src="<?=base_url().'./uploads/profilepic/profile'.$followDatas->user_id.'.jpg'?>" 
 						class="" style="height:50px;width:50px" alt="profile pic">
+					<?php } else {
+						?>
+						<img src="https://www.linkpicture.com/q/avatarprofile.png" type="image"
+							 class="avatar img-circle img-thumbnail" style="height:50px;width:50px">
+						<?php
+					} ?>
 				</a>
 				<div class="td p-r-hdr">
 					<div class="p-u-info">
