e6137e7da7783ec8b0fe1905890fd9fe1bffcb00
Ken Denzel <kendenzelcruz@gmail.com>
2021-05-03 12:26:11 +0800

    Update homepage.php

--- application/views/user/homepage.php
+++ application/views/user/homepage.php
@@ -216,10 +216,10 @@ section{
 							</span>
 						</div>
 						<div class="comment p-2 cursor col-md-4 pr-0" id="comment_id_<?= $post_id ?>"
-							 style="cursor: pointer; margin-right: 20px;"
+							 style="cursor: pointer;"
 							 data-post_id="<?= $post_id ?>">
 							<i class="fa fa-comment-o"></i>
-							<span class="ml-1"><a href="<?= base_url('user/viewFullDiet/' . $post_id) ?>"> Comments </a> </span> <span><?=(isset($row->getCommentCount) && ($row->getCommentCount)!=0)?$row->getCommentCount:''?></span>
+							<span style="font-size: 5px;" class="ml-1"><a href="<?= base_url('user/viewFullDiet/' . $post_id) ?>"> Comments </a> </span> <span><?=(isset($row->getCommentCount) && ($row->getCommentCount)!=0)?$row->getCommentCount:''?></span>
 						</div>
 					</div>
 				</div>
